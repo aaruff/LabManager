8773ffd64a59e5977d412605c361dff070d5341e
Anwar A. Ruff <anwarruff@gmail.com>
2012-10-29 23:58:05 -0400

    Added packetType and renamed getContent and setContent to getPayload and setPayload respectively.

--- src/main/java/edu/nyu/cess/remote/common/net/DataPacket.java
+++ src/main/java/edu/nyu/cess/remote/common/net/DataPacket.java
@@ -3,22 +3,31 @@ package edu.nyu.cess.remote.common.net;
 import java.io.Serializable;
 
 public class DataPacket implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	Object content;
 
-	public DataPacket(Object content) {
-		this.content = content;
+	private PacketType packetType;
+	
+	private Object payload;
+	
+	public DataPacket(PacketType packetType, Object content) {
+		this.payload = content;
+		this.packetType = packetType;
 	}
 
-	public Object getContent() {
-		return content;
+	public Object getPayload() {
+		return payload;
 	}
 
-	public void setContent(Object content) {
-		this.content = content;
+	public void setPayload(Object content) {
+		this.payload = content;
 	}
+	
 
+	public void setPacketType(PacketType packetType) {
+		this.packetType = packetType;
+	}
+	
+	public PacketType getPacketType() {
+		return this.packetType;
+	}
 }
