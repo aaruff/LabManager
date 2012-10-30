51c843b801335f56d3cd0551f88ad0ca203df480
Anwar A. Ruff <anwarruff@gmail.com>
2012-10-29 23:59:13 -0400

    Added HostInfo class

---
+++ src/main/java/edu/nyu/cess/remote/common/net/HostInfo.java
@@ -0,0 +1,24 @@
+package edu.nyu.cess.remote.common.net;
+
+import java.io.Serializable;
+
+public class HostInfo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public String getIPAddress() {
+		return IPAddress;
+	}
+	public void setIPAddress(String iPAddress) {
+		IPAddress = iPAddress;
+	}
+	private String hostName;
+	private String IPAddress;
+	
+
+}
