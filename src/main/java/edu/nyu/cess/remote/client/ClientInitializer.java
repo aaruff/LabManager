07cb986b6db5cc0c9503fe63dc94e564cf33ff8b
aaruff <anwarruff@gmail.com>
2015-11-06 15:23:34 -0500

    Added config file loading and validation.

--- src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
+++ src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
@@ -1,10 +1,36 @@
 package edu.nyu.cess.remote.client;
 
-public class ClientInitializer {
+import edu.nyu.cess.remote.client.config.ConfigFileValidator;
+import edu.nyu.cess.remote.client.config.HostConfigFile;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+public class ClientInitializer
+{
+	private final static Logger log = Logger.getLogger(ClientInitializer.class);
+
 
 	public static void main(String[] args) {
 		Client client = new Client();
-		client.initServerConnection();
+
+		HostConfigFile configFile;
+		try {
+			configFile = new HostConfigFile();
+		}
+		catch (Exception e) {
+			log.error("Failed to open the configuration file. Make sure that config.properties is in the classpath.");
+			System.exit(1);
+			return;
+		}
+
+		ConfigFileValidator validator = new ConfigFileValidator(configFile);
+		if ( ! validator.validate()) {
+			log.error(StringUtils.join(validator.getErrors(), ", "));
+			System.exit(1);
+			return;
+		}
+
+		client.initServerConnection(configFile);
 	}
 
 }
