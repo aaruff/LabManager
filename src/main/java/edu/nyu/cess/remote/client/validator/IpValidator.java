6e10d5c06d6d78b2e3557f3fa6e6653a59079e67
aaruff <anwarruff@gmail.com>
2015-11-06 15:26:11 -0500

    Newly created class

---
+++ src/main/java/edu/nyu/cess/remote/client/validator/IpValidator.java
@@ -0,0 +1,56 @@
+package edu.nyu.cess.remote.client.validator;
+
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+import java.util.ArrayList;
+
+/**
+ * IP Address Validator
+ */
+public class IpValidator extends Validator
+{
+	private String ip;
+
+	public IpValidator() {}
+
+	/**
+	 * Sets the ip instance variable.
+	 * @param ip
+     */
+	public IpValidator(String ip)
+	{
+		this.ip = ip;
+	}
+
+
+	/**
+	 * Validates the ip instance variable.
+	 *
+	 * @return boolean
+	 */
+	public boolean validate()
+	{
+		errors = new ArrayList<>();
+
+		if (ip == null) {
+			errors.add("IP address is null.");
+			return false;
+		}
+
+		InetAddressValidator validator = InetAddressValidator.getInstance();
+		if ( ! validator.isValid(ip)) {
+			errors.add("Invalidly formatted IP Address.");
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Sets the IP address string for validation.
+	 * @param ip
+	 */
+	public void setIpAddress(String ip)
+	{
+		this.ip = ip;
+	}
+}
