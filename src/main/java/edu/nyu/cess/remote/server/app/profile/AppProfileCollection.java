bf868cf4dc7bcc57fb9d9bd97fad3f85b2299c5e
aruff <aruff@128-122-11-143.DYNAPOOL.NYU.EDU>
2015-12-08 15:35:22 -0500

    Created app profile collection.

---
+++ src/main/java/edu/nyu/cess/remote/server/app/profile/AppProfileCollection.java
@@ -0,0 +1,35 @@
+package edu.nyu.cess.remote.server.app.profile;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An application configuration collection.
+ */
+public class AppProfileCollection implements Iterable<AppProfile>
+{
+	private List<AppProfile> appProfileCollection;
+
+	/**
+	 * Construct the collection
+	 *
+	 * @param appProfileCollection
+	 */
+	public AppProfileCollection(List<AppProfile> appProfileCollection)
+	{
+		this.appProfileCollection = appProfileCollection;
+	}
+
+	public static void sort(Object[] a, Comparator comparator)
+	{
+
+	}
+
+	@Override
+	public Iterator<AppProfile> iterator()
+	{
+		return appProfileCollection.iterator();
+	}
+
+}
