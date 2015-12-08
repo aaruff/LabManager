25a830fd8b712921b0b14cc972d6facae3692583
aruff <aruff@128-122-11-143.DYNAPOOL.NYU.EDU>
2015-12-08 15:31:40 -0500

    Created to read the server config file.

---
+++ src/main/java/edu/nyu/cess/remote/server/app/profile/AppProfilesFile.java
@@ -0,0 +1,31 @@
+package edu.nyu.cess.remote.server.app.profile;
+
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.error.YAMLException;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppProfilesFile
+{
+	/**
+	 * Reads the remotely executable application list.
+	 *
+	 * @param inputStream
+	 * @return List<RemoteExecProfile>
+	 * @throws YAMLException
+     */
+	public static Map<String, AppProfile> readFile(InputStream inputStream) throws YAMLException
+	{
+		Yaml yaml = new Yaml(new Constructor(AppProfile.class));
+
+		Map<String, AppProfile> appProfileList = new HashMap<>();
+		for (Object remoteExecProfile : yaml.loadAll(inputStream)) {
+			AppProfile appProfile = (AppProfile) remoteExecProfile;
+			appProfileList.put(appProfile.getName(), appProfile);
+		}
+		return appProfileList;
+	}
+}
