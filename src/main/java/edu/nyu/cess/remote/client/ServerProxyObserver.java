9cb58df208389d4552429b628621f8a9ac61a875
aruff <aruff@128-122-11-154.DYNAPOOL.NYU.EDU>
2015-12-17 18:49:48 -0500

    Updated renamed classes

--- src/main/java/edu/nyu/cess/remote/client/ServerProxyObserver.java
+++ src/main/java/edu/nyu/cess/remote/client/ServerProxyObserver.java
@@ -3,14 +3,14 @@
  */
 package edu.nyu.cess.remote.client;
 
-import edu.nyu.cess.remote.common.app.ExecutionRequest;
+import edu.nyu.cess.remote.common.app.ExeRequestMessage;
 
 /**
  * @author Anwar A. Ruff
  */
 public interface ServerProxyObserver {
 
-	public void updateServerExecutionRequestReceived(ExecutionRequest executionRequest);
+	public void updateServerExecutionRequestReceived(ExeRequestMessage exeRequestMessage);
 
 	public void updateNetworkStateChanged(boolean isConnected);
 
