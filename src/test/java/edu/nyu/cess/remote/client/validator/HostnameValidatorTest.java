251f31ccbb4961101820f2c638119d0fead4e0bb
aruff <aruff@128-122-11-154.DYNAPOOL.NYU.EDU>
2015-12-17 18:50:18 -0500

    Added class doc block.

--- src/test/java/edu/nyu/cess/remote/client/validator/HostnameValidatorTest.java
+++ src/test/java/edu/nyu/cess/remote/client/validator/HostnameValidatorTest.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by aruff on 11/6/15.
- */
 public class HostnameValidatorTest
 {
 	@Test
