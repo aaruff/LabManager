2f13053ace3cd800afd12d88050c14875d188bf0
aruff <aruff@128-122-11-154.DYNAPOOL.NYU.EDU>
2015-12-17 18:48:04 -0500

    Created host config interface

---
+++ src/main/java/edu/nyu/cess/remote/client/config/HostConfigInterface.java
@@ -0,0 +1,23 @@
+package edu.nyu.cess.remote.client.config;
+
+/**
+ * The interface for host config information.
+ */
+public interface HostConfigInterface
+{
+	/**
+	 * @return String IP address
+     */
+	String getIpAddress();
+
+	/**
+	 * @return String port
+     */
+	String getPort();
+
+	/**
+	 *
+	 * @return String host name
+     */
+	String getHostName();
+}
