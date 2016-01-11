3f5b42563e71a9052b0afbe542500a39f685537e
aruff <aruff@128-122-11-169.DYNAPOOL.NYU.EDU>
2016-01-11 13:48:17 -0500

    Minor reformatting

--- src/main/java/edu/nyu/cess/remote/common/net/HostInfo.java
+++ src/main/java/edu/nyu/cess/remote/common/net/HostInfo.java
@@ -2,23 +2,30 @@ package edu.nyu.cess.remote.common.net;
 
 import java.io.Serializable;
 
-public class HostInfo implements Serializable {
+public class HostInfo implements Serializable
+{
 	private static final long serialVersionUID = 1L;
-	
-	public String getHostName() {
-		return hostName;
-	}
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
-	public String getIPAddress() {
-		return IPAddress;
-	}
-	public void setIPAddress(String iPAddress) {
-		IPAddress = iPAddress;
-	}
 	private String hostName;
 	private String IPAddress;
-	
 
+
+	public String getHostName()
+	{
+		return hostName;
+	}
+
+	public void setHostName(String hostName)
+	{
+		this.hostName = hostName;
+	}
+
+	public String getIPAddress()
+	{
+		return IPAddress;
+	}
+
+	public void setIPAddress(String iPAddress)
+	{
+		IPAddress = iPAddress;
+	}
 }
