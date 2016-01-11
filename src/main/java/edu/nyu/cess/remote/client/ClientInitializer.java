cbc0b301c706a314fb927c2d7e5ce9ef4a58f65a
aruff <aruff@128-122-11-169.DYNAPOOL.NYU.EDU>
2016-01-11 16:42:28 -0500

    refactored to use HostConfigFile.readPropertyFile

--- src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
+++ src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
@@ -1,5 +1,6 @@
 package edu.nyu.cess.remote.client;
 
+import edu.nyu.cess.remote.common.net.ClientServerNetworkInfo;
 import edu.nyu.cess.remote.client.config.ConfigFileValidator;
 import edu.nyu.cess.remote.client.config.HostConfigFile;
 import org.apache.commons.lang.StringUtils;
@@ -9,13 +10,16 @@ public class ClientInitializer
 {
 	private final static Logger log = Logger.getLogger(ClientInitializer.class);
 
-
+	/**
+	 * Reads in the client config file, and starts the client.
+	 *
+	 * @param args command line arguments
+     */
 	public static void main(String[] args) {
-		Client client = new Client();
 
-		HostConfigFile configFile;
+		ClientServerNetworkInfo configFile;
 		try {
-			configFile = new HostConfigFile();
+			configFile = HostConfigFile.readPropertyFile("config.properties");
 		}
 		catch (Exception e) {
 			log.error("Failed to open the configuration file. Make sure that config.properties is in the classpath.");
@@ -30,6 +34,7 @@ public class ClientInitializer
 			return;
 		}
 
+		Client client = new Client();
 		client.initServerConnection(configFile);
 	}
 
