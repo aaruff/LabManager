a9e996e9485574fd84b9161b3747ee3d18e7529c
Anwar Ruff <anwarruff@gmail.com>
2016-01-19 15:23:21 -0500

    Replaced upcasting to object with direct references to used classes

--- src/main/java/edu/nyu/cess/remote/common/net/Message.java
+++ src/main/java/edu/nyu/cess/remote/common/net/Message.java
@@ -1,24 +1,51 @@
 package edu.nyu.cess.remote.common.net;
 
+import edu.nyu.cess.remote.common.app.AppExecution;
+
 import java.io.Serializable;
 
 public class Message implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private MessageType messageType;
+	private NetworkInformation networkInfo;
+	private AppExecution appExecution;
+	private String clientMessage;
 
-	private Object payload;
-
-	public Message(MessageType messageType, Object content) {
-		this.payload = content;
+	public Message(MessageType messageType, AppExecution appExecution, NetworkInformation networkInfo) {
 		this.messageType = messageType;
+		this.appExecution = appExecution;
+		this.networkInfo = networkInfo;
 	}
 
-	public Object getPayload() {
-		return payload;
+	public Message(MessageType messageType, String clientMessage)
+	{
+		this.messageType = messageType;
+		this.clientMessage = clientMessage;
+	}
+
+	public Message(MessageType messageType, NetworkInformation networkInfo)
+	{
+		this.messageType = messageType;
+		this.networkInfo = networkInfo;
 	}
 
 	public MessageType getMessageType() {
 		return this.messageType;
+	}
+
+	public NetworkInformation getNetworkInfo()
+	{
+		return networkInfo;
+	}
+
+	public AppExecution getAppExecution()
+	{
+		return appExecution;
+	}
+
+	public String getClientMessage()
+	{
+		return clientMessage;
 	}
 }
