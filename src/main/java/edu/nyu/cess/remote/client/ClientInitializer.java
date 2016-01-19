c50af8ca55b455c41c6669ec08903d4f049401b3
Anwar Ruff <anwarruff@gmail.com>
2016-01-19 15:20:24 -0500

    Updated to reflect refactored classes

--- src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
+++ src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
@@ -1,8 +1,10 @@
 package edu.nyu.cess.remote.client;
 
-import edu.nyu.cess.remote.common.net.ClientServerNetworkInfo;
-import edu.nyu.cess.remote.client.config.ConfigFileValidator;
-import edu.nyu.cess.remote.client.config.HostConfigFile;
+import edu.nyu.cess.remote.client.config.NetworkInformationFile;
+import edu.nyu.cess.remote.client.config.NetworkInformationFileValidator;
+import edu.nyu.cess.remote.client.net.NetworkMessageHandler;
+import edu.nyu.cess.remote.client.notification.UserNotifier;
+import edu.nyu.cess.remote.common.net.NetworkInformation;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
@@ -15,27 +17,25 @@ public class ClientInitializer
 	 *
 	 * @param args command line arguments
      */
-	public static void main(String[] args) {
-
-		ClientServerNetworkInfo clientServerNetworkInfo;
+	public static void main(String[] args)
+    {
 		try {
-			clientServerNetworkInfo = HostConfigFile.readPropertyFile("config.properties");
+			NetworkInformation networkInfo = NetworkInformationFile.readPropertyFile("config.properties");
+
+            NetworkInformationFileValidator validator = new NetworkInformationFileValidator(networkInfo);
+            if ( ! validator.validate()) {
+                log.error(StringUtils.join(validator.getErrors(), ", "));
+                System.exit(1);
+                return;
+            }
+
+			NetworkMessageHandler networkMessageHandler = new NetworkMessageHandler(networkInfo, new UserNotifier());
+            networkMessageHandler.startMessageHandler();
 		}
 		catch (Exception e) {
 			log.error("Failed to open the configuration file. Make sure that config.properties is in the classpath.");
 			System.exit(1);
 			return;
 		}
-
-		ConfigFileValidator validator = new ConfigFileValidator(clientServerNetworkInfo);
-		if ( ! validator.validate()) {
-			log.error(StringUtils.join(validator.getErrors(), ", "));
-			System.exit(1);
-			return;
-		}
-
-		Client client = new Client();
-		client.initServerConnection(clientServerNetworkInfo);
 	}
-
 }
