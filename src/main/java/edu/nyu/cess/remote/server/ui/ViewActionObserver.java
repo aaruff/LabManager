163e7664ce8b8aebade774c9b9f2e2e36c8d04dc
Anwar A. Ruff <anwarruff@gmail.com>
2016-01-22 13:26:43 -0500

    Renamed function

--- src/main/java/edu/nyu/cess/remote/server/ui/ViewActionObserver.java
+++ src/main/java/edu/nyu/cess/remote/server/ui/ViewActionObserver.java
@@ -5,7 +5,7 @@ public interface ViewActionObserver
 	void notifyViewObserverStartAppInRangeRequested(String app, String start, String end);
 	void messageClient(String message, String ipAddress);
 	void messageClientInRange(String message, String lowerBoundHostName, String upperBoundHostName);
-	void stopAppInRange(String start, String end);
+	void notifyStopAppInRangeRequested(String start, String end);
 	void startApplication(String appName, String ipAddress);
 	void stopApplication(String ipAddress);
 }
