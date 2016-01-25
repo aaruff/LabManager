3f5e65c359ba22196929e61f00a13c9b9f61bab2
Anwar Ruff <anwarruff@gmail.com>
2016-01-25 15:56:42 -0500

    Refactored message sending/receiving code into new class from ServerMessageHandler

---
+++ src/main/java/edu/nyu/cess/remote/client/net/MessageSocketHandler.java
@@ -0,0 +1,53 @@
+package edu.nyu.cess.remote.client.net;
+
+import edu.nyu.cess.remote.common.net.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * Created by aruff on 1/25/16.
+ */
+public class MessageSocketHandler implements MessageSocket
+{
+	private final Socket socket;
+
+	public MessageSocketHandler(String ipAddress, int port) throws IOException
+	{
+		socket = new Socket(ipAddress, port);
+	}
+
+	@Override public synchronized boolean isConnected()
+	{
+		return socket != null && socket.isConnected() && ! socket.isClosed();
+	}
+
+
+	@Override public synchronized void sendMessage(Message packet) throws IOException
+	{
+		if ( ! isConnected()) {
+			throw new IOException("Error attempting to send a message with an invalid socket");
+		}
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		objectOutputStream.writeObject(packet);
+		objectOutputStream.flush();
+	}
+
+	@Override public synchronized Message readMessage() throws IOException {
+		if ( ! isConnected()) {
+			throw new IOException("Error attempting to read a message with an invalid socket");
+		}
+
+		ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+		Object object;
+		try {
+			object = objectInputStream.readObject();
+		} catch (ClassNotFoundException e) {
+			throw new IOException("Error reading object from socket input stream", e);
+		}
+
+		return (Message) object;
+	}
+}
--- src/main/java/edu/nyu/cess/remote/client/net/ServerMessageHandler.java
+++ src/main/java/edu/nyu/cess/remote/client/net/ServerMessageHandler.java
@@ -9,49 +9,75 @@ import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.net.ConnectException;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class ServerMessageHandler implements MessageHandlerController, ApplicationStateObserver
+public class ServerMessageHandler implements NetworkInterfaceObserver, ApplicationStateObserver
 {
-	private java.net.Socket socket;
-
-	private NetworkInformation networkInformation;
-
-	private AppExecutionHandler appExecutionHandler;
-	private UserNotificationHandler userNotificationHandler;
-
-	private ObjectInputStream objectInputStream;
-	private ObjectOutputStream objectOutputStream;
-
 	final static Logger log = Logger.getLogger(ServerMessageHandler.class);
 
-	public ServerMessageHandler(NetworkInformation networkInformation, UserNotificationHandler userNotificationHandler)
+	private final NetworkInformation networkInfo;
+
+	private final AppExecutionHandler appExecHandler;
+	private final UserNotificationHandler userNotificationHandler;
+
+	private MessageSocket messageSocket;
+
+	public ServerMessageHandler(NetworkInformation networkInfo, UserNotificationHandler userNotificationHandler)
 	{
-		this.appExecutionHandler = new AppExecutor(this);
-		this.networkInformation = networkInformation;
+		this.appExecHandler = new AppExecutor(getApplicationStateObserverFrom(this));
+		this.networkInfo = networkInfo;
 		this.userNotificationHandler = userNotificationHandler;
 	}
 
-	/**
-	 * {@link MessageHandlerController}
-	 */
-	@Override public void initServerMessageListener() {
+	public void start(NetworkInformation networkInfo)
+    {
+        // Start the network interface monitor
+        Thread netInterfaceMonitorThread = new Thread(new NetworkInterfaceMonitor(getNetworkInterfaceObserverFrom(this), networkInfo));
+        netInterfaceMonitorThread.setName("Network Interface Monitor");
+        netInterfaceMonitorThread.start();
 
+        // Continuously attempt to establish a socket connection to the server
 		while (true) {
-			openSocket();
-            confirmNetworkInfo();
-            startNetworkInterfaceMonitor();
-			startSocketListenerThread();
+			try {
+				messageSocket = new MessageSocketHandler(networkInfo.getServerIpAddress(), networkInfo.getServerPort());
+			} catch (IOException e) {
+				log.error("Error: Failed to create a message socket.");
+			}
+
+			while (messageSocket.isConnected()) {
+                try {
+                    routeIncomingMessage(messageSocket.readMessage(), networkInfo);
+                }
+                catch (ClassNotFoundException|IOException e) {
+                    log.error("Error reading message", e);
+                }
+            }
+
+			// Wait 2 minutes before trying to create another socket
+			try {
+				int milliseconds = 2000;
+				Thread.sleep(milliseconds);
+			} catch (InterruptedException e) {
+				log.error("Thread sleep interrupted.", e);
+			}
 		}
 	}
 
 	/**
-	 * {@link MessageHandlerController}
+	 * {@link NetworkInterfaceObserver}
 	 */
-	@Override public synchronized void stopMessageHandler() {
-		close(socket);
+    @Override public synchronized void notifyNetworkInterfaceDown()
+    {
+        stop();
+
+    }
+
+	/**
+	 * {@link NetworkInterfaceObserver}
+	 */
+	private synchronized void stop() {
 		close(objectOutputStream);
-		close(objectInputStream);
 	}
 
 	/**
@@ -59,7 +85,7 @@ public class ServerMessageHandler implements MessageHandlerController, Applicati
 	 */
 	@Override public void applicationStateUpdate(AppExecution appExecution)
 	{
-		Message message = new Message(MessageType.STATE_CHANGE, appExecution, networkInformation);
+		Message message = new Message(MessageType.STATE_CHANGE, appExecution, networkInfo);
 		sendMessage(message);
 	}
 
@@ -67,240 +93,83 @@ public class ServerMessageHandler implements MessageHandlerController, Applicati
 	 *                       PRIVATE
 	 * ---------------------------------------------------- */
 
-	/**
+    /**
 	 * Sends the provided message to the server.
 	 *
 	 * @param message The message
      */
-	private void routeIncomingMessage(Message message)
+	private void routeIncomingMessage(Message message, NetworkInformation netInfo)
 	{
-		log.info("Server message received.");
-
 		switch(message.getMessageType()) {
 			case APPLICATION_EXECUTION:
-				AppExecution appExecution = message.getAppExecution();
-				if (appExecution != null) {
-					log.info("Packet Content: ApplicationExecRequest");
-					appExecutionHandler.executeRequest(appExecution);
-				}
+                if ( ! AppExecutionValidator.validate(message.getAppExecution())) {
+                    log.error("Invalid application execution received and ignored.");
+                    return;
+                }
+
+                appExecHandler.executeRequest(message.getAppExecution());
 				break;
 			case USER_NOTIFICATION:
 				String text = message.getClientMessage();
-				if (text != null && !text.isEmpty()) {
-					userNotificationHandler.notifyUser(text);
+				if (text == null || text.isEmpty()) {
+                    log.error("Invalid client message received and ignored.");
 				}
+
+                userNotificationHandler.notifyUser(text);
 				break;
+            case NET_INFO_REQUEST:
+                sendMessage(new Message(MessageType.NET_INFO_RESPONSE, appExecHandler.getAppExecution(), netInfo));
+                break;
+            // Unsupported messages on client ignored
 			case STATE_CHANGE:
-			case NETWORK_INFO_UPDATE:
-				// Not supported by the Client
-				break;
 			case KEEP_ALIVE_PING:
-				// No processing occurs during a socket test
-				break;
 			default:
-				// Do nothing
 				break;
 		}
-	}
-
-	/**
-	 * Sends a {@link Message} to the Server
-	 *
-	 * @param packet
-	 *            A data packet wrapper
-	 * @return true if a connection was established, otherwise false
-	 */
-	private boolean sendMessage(Message packet) {
-		boolean streamInitialized = false;
-
-		if (socket != null) {
-			if (socket.isConnected()) {
-
-				if (objectOutputStream == null) {
-					streamInitialized = setServerObjectOutputStream();
-				}
-
-				if (objectOutputStream != null) {
-					try {
-						objectOutputStream.writeObject(packet);
-						objectOutputStream.flush();
-					} catch (IOException e) {
-						log.error(e);
-					}
-				}
-			}
-		}
-
-		return streamInitialized;
-	}
-
-	private synchronized Message readDataPacket() {
-		Message message = null;
-		boolean streamInitialized = true;
-
-		if (socket != null) {
-			if (socket.isConnected()) {
-
-				if (objectInputStream == null) {
-					streamInitialized = setServerObjectInputStream();
-				}
-
-				if (objectInputStream != null && streamInitialized) {
-					try {
-						Object object = objectInputStream.readObject();
-						message = (Message) object;
-					} catch (ClassNotFoundException e) {
-						log.error("The Serialized Object Not Found", e);
-						message = null;
-					} catch (StreamCorruptedException e) {
-						log.error(e);
-						message = null;
-					} catch (IOException e) {
-						log.error(e);
-						message = null;
-					}
-				}
-			}
-		}
-
-		return message;
-
-	}
-
-	/**
-	 * Initialize the {@link ObjectInputStream}.
-	 *
-	 * @return an {@link ObjectInputStream} or <code>null</code> if
-	 *         initialization failed.
-	 */
-	private boolean setServerObjectInputStream() {
-		boolean result = false;
-
-		if (this.socket.isConnected()) {
-			try {
-				InputStream inputStream = this.socket.getInputStream();
-				if (inputStream != null) {
-					this.objectInputStream = new ObjectInputStream(inputStream);
-					result = true;
-				}
-			} catch (IOException ex) {
-				this.objectInputStream = null;
-				result = false;
-			}
-		}
-
-		return result;
-	}
-
-	/**
-	 * Initialize the {@link ObjectOutputStream}
-	 *
-	 * @return an {@link ObjectOutputStream} or null if initialization failed.
-	 */
-	private boolean setServerObjectOutputStream() {
-		boolean result = false;
-
-		if (socket != null) {
-			try {
-				OutputStream outputStream = socket.getOutputStream();
-				if (outputStream != null) {
-					objectOutputStream = new ObjectOutputStream(outputStream);
-					result = true;
-				}
-			} catch (IOException e) {
-				log.error("Error occurred retrieving ObjectOutputStream.", e);
-			}
-		}
-		return result;
-	}
-
-	/**
-	 * Initializes a {@link java.net.Socket} connection via the IP Address and Port
-	 * Number. If the initialization of the Socket fails the return value will
-	 * be null.
-	 *
-	 * @return initialized {@link java.net.Socket}, or null if a socket connection is not
-	 *         established.
-	 */
-	private java.net.Socket getServerSocketConnection() {
-		socket = null;
-		try {
-			socket = new java.net.Socket(networkInformation.getServerIpAddress(), networkInformation.getServerPort());
-
-			log.info("Network connection established...");
-
-		} catch (UnknownHostException e) {
-			log.error("Error: No Known Host.", e);
-
-		} catch (ConnectException e) {
-			log.error("Error: Failed to connection to server.", e);
-
-		} catch (IOException e) {
-			log.error("IO Exception occurred.", e);
-		}
-
-		return socket;
 	}
 
 	/**
 	 * Polls the remote network node on a millisecond interval until a
-	 * {@link java.net.Socket} connection is established.
+	 * {@link Socket} connection is established.
 	 */
-	private void openSocket() {
-		int pollInterval = 2000; // milliseconds
-		socket = null;
+	private MessageSocket getSocket(String serverIpAddress, NetworkInformation networkInfo) {
 
-		socket = getServerSocketConnection();
-		while (socket == null) {
-			try {
-				Thread.sleep(pollInterval);
-				log.info("Attempting to connect to server: " + networkInformation.getServerIpAddress());
-				socket = getServerSocketConnection();
-			} catch (InterruptedException e) {
-				log.error("Thread sleep interrupted.", e);
-			}
-		}
-	}
+        Socket socket = null;
+        while (true) {
+            try {
+                socket = new Socket(serverIpAddress, networkInfo.getServerPort());
+                return new MessageSocketHandler(socket, networkInfo);
+            } catch (UnknownHostException e) {
+                log.error("Error: No Known Host.", e);
 
-    private void confirmNetworkInfo()
-    {
-        log.info("Sending initial client-server network info to the server.");
-        Message message = new Message(MessageType.NETWORK_INFO_UPDATE, networkInformation);
-        sendMessage(message);
+            } catch (ConnectException e) {
+                log.error("Error: Failed to connection to server.", e);
 
-        boolean infoConfirmed = false;
-        while (! infoConfirmed) {
-            message = readDataPacket();
-            if (message.getMessageType() == MessageType.NETWORK_INFO_CONFIRMED) {
-                infoConfirmed = true;
+            } catch (IOException e) {
+                log.error("IO Exception occurred.", e);
+            }
+            finally {
+                if (socket == null) {
+                    try {
+                        int milliseconds = 2000;
+                        Thread.sleep(milliseconds);
+                    } catch (InterruptedException e) {
+                        log.error("Thread sleep interrupted.", e);
+                    }
+                }
             }
         }
-
-        close(objectOutputStream);
-        close(objectInputStream);
-    }
-
-    private void startNetworkInterfaceMonitor()
-    {
-        log.info("Starting the network interface monitor.");
-        Thread networkInterfaceMonitorThread = new Thread(new NetworkInterfaceMonitor(this, networkInformation));
-        networkInterfaceMonitorThread.setName("Network Interface Monitor");
-        networkInterfaceMonitorThread.start();
-    }
-
-	private void startSocketListenerThread()
-	{
-        //TODO: Refactor out this function and place it into a thread that performs callback on the MessageHandler on incoming messages
-		log.info("Waiting for message from the server.");
-        Message message;
-		while ((message = readDataPacket()) != null) {
-            routeIncomingMessage(message);
-		}
-
-		close(objectOutputStream);
-		close(objectInputStream);
-
 	}
+
+    private ApplicationStateObserver getApplicationStateObserverFrom(ServerMessageHandler serverMessageHandler)
+    {
+        return serverMessageHandler;
+    }
+
+    private NetworkInterfaceObserver getNetworkInterfaceObserverFrom(ServerMessageHandler serverMessageHandler)
+    {
+        return serverMessageHandler;
+    }
 
 	private void close(Closeable closeable)
 	{
