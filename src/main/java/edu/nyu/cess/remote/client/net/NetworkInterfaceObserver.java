a02a6a56c7374ee045ba0845a7ea7625499603f1
Anwar Ruff <anwarruff@gmail.com>
2016-01-25 15:52:58 -0500

    Renamed class

---
+++ src/main/java/edu/nyu/cess/remote/client/net/NetworkInterfaceObserver.java
@@ -0,0 +1,6 @@
+package edu.nyu.cess.remote.client.net;
+
+public interface NetworkInterfaceObserver
+{
+	void notifyNetworkInterfaceDown();
+}
--- src/main/java/edu/nyu/cess/remote/client/net/ServerMessageMonitorState.java
+++
@@ -1,6 +0,0 @@
-package edu.nyu.cess.remote.client.net;
-
-public enum ServerMessageMonitorState
-{
-    NOT_CONNECTED_TO_SERVER, CONNECTED_TO_SERVER, NETWORK_INFO_CONFIRMED
-}
