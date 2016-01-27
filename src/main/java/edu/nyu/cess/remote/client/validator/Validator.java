b10886800da4cc285b683a46bbf294216ffa209c
Anwar Ruff <anwarruff@gmail.com>
2016-01-27 17:20:23 -0500

    Removed unused functions

--- src/main/java/edu/nyu/cess/remote/client/validator/Validator.java
+++ src/main/java/edu/nyu/cess/remote/client/validator/Validator.java
@@ -2,30 +2,8 @@ package edu.nyu.cess.remote.client.validator;
 
 import java.util.ArrayList;
 
-/**
- * Created by aruff on 11/5/15.
- */
-public abstract class Validator
+public interface Validator
 {
-	protected ArrayList<String> errors;
-
-	public abstract boolean validate();
-
-	/**
-	 * Clear errors.
-	 */
-	protected void clearErrors()
-	{
-		errors = new ArrayList<>();
-	}
-
-	/**
-	 * Return errors
-	 *
-	 * @return String
-	 */
-	public ArrayList<String> getErrors()
-	{
-		return errors;
-	}
+	boolean validate();
+	ArrayList<String> getErrors();
 }
