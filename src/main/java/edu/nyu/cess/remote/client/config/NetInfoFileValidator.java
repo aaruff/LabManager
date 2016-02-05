1e02306d5c4c5a36df897a12d9773d9265f6bdf9
Anwar A. Ruff <anwarruff@gmail.com>
2016-02-05 16:40:32 -0500

    Renamed class

--- src/main/java/edu/nyu/cess/remote/client/config/NetInfoFileValidator.java
+++ src/main/java/edu/nyu/cess/remote/client/config/NetInfoFileValidator.java
@@ -1,10 +1,11 @@
 package edu.nyu.cess.remote.client.config;
 
+import edu.nyu.cess.remote.common.net.PortInfo;
 import edu.nyu.cess.remote.client.validator.HostNameValidator;
 import edu.nyu.cess.remote.client.validator.IpValidator;
 import edu.nyu.cess.remote.client.validator.PortValidator;
 import edu.nyu.cess.remote.client.validator.Validator;
-import edu.nyu.cess.remote.common.net.NetworkInformation;
+import edu.nyu.cess.remote.common.net.NetworkInfo;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
@@ -13,40 +14,47 @@ import java.util.ArrayList;
 /**
  * Validates the config file properties.
  */
-public class NetworkInformationFileValidator implements Validator
+public class NetInfoFileValidator implements Validator
 {
-	private NetworkInformation networkInformation;
+	private NetInfoFile netInfoFile;
 	private ArrayList<String> errors;
 
-	public NetworkInformationFileValidator(NetworkInformation networkInformation)
+	public NetInfoFileValidator(NetInfoFile netInfoFile)
 	{
-		this.networkInformation = networkInformation;
+		this.netInfoFile = netInfoFile;
 	}
 
 	@Override public boolean validate()
 	{
 		clearErrors();
+		NetworkInfo netInfo = netInfoFile.getNetworkInfo();
+		PortInfo portInfo = netInfoFile.getPortInfo();
 
-		IpValidator ipValidator = new IpValidator(networkInformation.getServerIpAddress());
+		IpValidator ipValidator = new IpValidator(netInfo.getServerIpAddress());
 		if ( ! ipValidator.validate()) {
 			errors.addAll(ipValidator.getErrors());
 			return false;
 		}
 
-		PortValidator portValidator = new PortValidator(networkInformation.getServerPort());
+		PortValidator portValidator = new PortValidator(portInfo.getNumber());
 		if ( ! portValidator.validate()) {
 			errors.addAll(portValidator.getErrors());
 			return false;
 		}
 
 
-		HostNameValidator hostNameValidator = new HostNameValidator(networkInformation.getClientName());
+		HostNameValidator hostNameValidator = new HostNameValidator(netInfo.getClientName());
 		if ( ! hostNameValidator.validate()) {
 			errors.addAll(hostNameValidator.getErrors());
 			return false;
 		}
 
 		return true;
+	}
+
+	public void setNetworkInfoFile(NetInfoFile netInfoFile)
+	{
+		this.netInfoFile = netInfoFile;
 	}
 
 	@Override public ArrayList<String> getErrors()
