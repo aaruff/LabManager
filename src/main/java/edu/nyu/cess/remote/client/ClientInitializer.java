4b42b32af3f585e7accba7aeab97f0740b7ab037
Anwar A. Ruff <anwarruff@gmail.com>
2016-02-05 16:46:13 -0500

    Added message routing and socket initialization

--- src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
+++ src/main/java/edu/nyu/cess/remote/client/ClientInitializer.java
@@ -1,15 +1,16 @@
 package edu.nyu.cess.remote.client;
 
 import edu.nyu.cess.remote.client.app.AppMessenger;
-import edu.nyu.cess.remote.client.config.NetworkInformationFile;
-import edu.nyu.cess.remote.client.config.NetworkInformationFileValidator;
+import edu.nyu.cess.remote.client.app.process.AppExecutionManager;
+import edu.nyu.cess.remote.client.config.NetInfoFile;
+import edu.nyu.cess.remote.client.config.NetInfoFileValidator;
+import edu.nyu.cess.remote.common.net.PortInfo;
 import edu.nyu.cess.remote.client.net.message.InboundMessageRouter;
 import edu.nyu.cess.remote.client.net.socket.SocketManager;
 import edu.nyu.cess.remote.client.notification.UserPrompt;
 import edu.nyu.cess.remote.client.notification.UserPromptMessenger;
-import edu.nyu.cess.remote.client.app.process.ProcessExecutionManager;
 import edu.nyu.cess.remote.common.net.MessageType;
-import edu.nyu.cess.remote.common.net.NetworkInformation;
+import edu.nyu.cess.remote.common.net.NetworkInfo;
 import org.apache.log4j.Logger;
 
 public class ClientInitializer
@@ -23,34 +24,40 @@ public class ClientInitializer
      */
 	public static void main(String[] args)
     {
+        NetInfoFile netInfoFile;
 		try {
-			NetworkInformation networkInfo = NetworkInformationFile.readPropertyFile("config.properties");
-
-            NetworkInformationFileValidator validator = new NetworkInformationFileValidator(networkInfo);
-            if ( ! validator.validate()) {
-                log.error(validator.getAllErrors());
-                System.exit(1);
-                return;
-            }
-
-			InboundMessageRouter messageRouter = new InboundMessageRouter();
-			SocketManager socketManager = new SocketManager(messageRouter, networkInfo);
-
-			ProcessExecutionManager appExecutionManager = new ProcessExecutionManager();
-			AppMessenger appMessenger = new AppMessenger(new ProcessExecutionManager(), socketManager, networkInfo);
-			appExecutionManager.setStateObserver(appMessenger);
-			messageRouter.setMessageHandler(MessageType.APPLICATION_EXECUTION, appMessenger);
-			messageRouter.setMessageHandler(MessageType.APPLICATION_STATE_UPDATE, appMessenger);
-
-			UserPromptMessenger userPromptMessenger = new UserPromptMessenger(new UserPrompt());
-			messageRouter.setMessageHandler(MessageType.USER_NOTIFICATION, userPromptMessenger);
-
-			socketManager.startPersistentConnection();
+            netInfoFile = new NetInfoFile("config.properties");
 		}
 		catch (Exception e) {
-			log.error("Failed to open the configuration file. Make sure that config.properties is in the classpath.");
+			log.error("Failed to open the configuration file. Make sure that config.properties is in the classpath.", e);
 			System.exit(1);
 			return;
 		}
+
+		NetInfoFileValidator netInfoValidator = new NetInfoFileValidator(netInfoFile);
+		if ( ! netInfoValidator.validate()) {
+			log.error(netInfoValidator.getAllErrors());
+			System.exit(1);
+			return;
+		}
+
+        PortInfo portInfo = netInfoFile.getPortInfo();
+        NetworkInfo networkInfo = netInfoFile.getNetworkInfo();
+
+        InboundMessageRouter inboundMessageRouter = new InboundMessageRouter();
+        SocketManager socketManager = new SocketManager(inboundMessageRouter, networkInfo, portInfo);
+
+        AppExecutionManager appExecutionManager = new AppExecutionManager();
+        AppMessenger appMessenger = new AppMessenger(new AppExecutionManager(), socketManager, networkInfo);
+
+        appExecutionManager.setStateObserver(appMessenger);
+
+        inboundMessageRouter.setInboundHandlerForMessageType(MessageType.APPLICATION_EXECUTION, appMessenger);
+        inboundMessageRouter.setInboundHandlerForMessageType(MessageType.APPLICATION_STATE_UPDATE, appMessenger);
+
+        UserPromptMessenger userPromptMessenger = new UserPromptMessenger(new UserPrompt());
+        inboundMessageRouter.setInboundHandlerForMessageType(MessageType.USER_NOTIFICATION, userPromptMessenger);
+
+        socketManager.startListeningForMessagesAndNotifyRouter();
 	}
 }
