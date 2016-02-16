bcc33ff30913776a38b0fe16386cb97e6cacc7c1
Anwar Ruff <anwarruff@gmail.com>
2016-02-16 15:07:07 -0500

    Added constructor and equality checking function

--- src/main/java/edu/nyu/cess/remote/common/app/AppExe.java
+++ src/main/java/edu/nyu/cess/remote/common/app/AppExe.java
@@ -2,9 +2,9 @@ package edu.nyu.cess.remote.common.app;
 
 import java.io.Serializable;
 
-public class AppExe implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class AppExe implements Serializable
+{
+	private static final long serialVersionUID = -3416367781462816040L;
 
 	private final String name;
 	private final String path;
@@ -17,6 +17,14 @@ public class AppExe implements Serializable {
 		this.path = path;
 		this.args = args;
 
+		this.appState = appState;
+	}
+
+	public AppExe(AppInfo appInfo, AppState appState)
+	{
+		this.name = appInfo.getName();
+		this.path = appInfo.getPath();
+		this.args = appInfo.getOptions();
 		this.appState = appState;
 	}
 
@@ -35,4 +43,15 @@ public class AppExe implements Serializable {
 	public String getArgs() {
 		return args;
 	}
+
+    public boolean isSameApp(AppExe appExe)
+    {
+        return name.equals(appExe.getName()) && path.equals(appExe.getPath()) && args.equals(appExe.getArgs());
+    }
+
+    public boolean isSameState(AppExe appExe)
+    {
+        return appExe.getState() == appState;
+    }
+
 }
