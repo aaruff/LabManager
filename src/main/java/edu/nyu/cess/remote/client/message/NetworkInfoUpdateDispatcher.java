928fc12dc552bb3d8a7b69b578b3ae582760c158
Anwar Ruff <anwarruff@gmail.com>
2016-02-23 15:29:48 -0500

    Added logging

--- src/main/java/edu/nyu/cess/remote/client/message/NetworkInfoUpdateDispatcher.java
+++ src/main/java/edu/nyu/cess/remote/client/message/NetworkInfoUpdateDispatcher.java
@@ -5,12 +5,14 @@ import edu.nyu.cess.remote.common.message.MessageType;
 import edu.nyu.cess.remote.common.message.dispatch.DispatchControl;
 import edu.nyu.cess.remote.common.message.dispatch.MessageDispatcher;
 import edu.nyu.cess.remote.common.net.NetworkInfo;
+import org.apache.log4j.Logger;
 
 /**
  * Created by aruff on 2/12/16.
  */
 public class NetworkInfoUpdateDispatcher implements MessageDispatcher
 {
+	private final static Logger log = Logger.getLogger(NetworkInfoUpdateDispatcher.class);
     private NetworkInfo networkInfo;
     private DispatchControl dispatchControl;
 
@@ -27,6 +29,7 @@ public class NetworkInfoUpdateDispatcher implements MessageDispatcher
 
     @Override public void dispatchMessage(Message message)
     {
+		log.info("Dispatching client network info to server");
         dispatchControl.dispatchOutboundMessage(new Message(MessageType.NETWORK_INFO_UPDATE, networkInfo));
     }
 }
