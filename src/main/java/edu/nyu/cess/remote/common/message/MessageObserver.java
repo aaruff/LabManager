51284313966a5c2251ad83a590b28acea4150528
Anwar Ruff <anwarruff@gmail.com>
2016-02-23 15:29:13 -0500

    Added network info to despatchInboundMessage

--- src/main/java/edu/nyu/cess/remote/common/message/MessageObserver.java
+++ src/main/java/edu/nyu/cess/remote/common/message/MessageObserver.java
@@ -1,9 +1,11 @@
 package edu.nyu.cess.remote.common.message;
 
+import edu.nyu.cess.remote.common.net.NetworkInfo;
+
 /**
  * Created by aruff on 2/8/16.
  */
 public interface MessageObserver
 {
-    void notifyMessageReceived(Message message);
+    void notifyMessageReceived(NetworkInfo networkInfo, Message message);
 }
--- src/main/java/edu/nyu/cess/remote/common/message/dispatch/DispatchControl.java
+++ src/main/java/edu/nyu/cess/remote/common/message/dispatch/DispatchControl.java
@@ -2,6 +2,7 @@ package edu.nyu.cess.remote.common.message.dispatch;
 
 import edu.nyu.cess.remote.common.message.Message;
 import edu.nyu.cess.remote.common.message.MessageType;
+import edu.nyu.cess.remote.common.net.NetworkInfo;
 
 /**
  * Created by aruff on 1/26/16.
@@ -24,7 +25,8 @@ public interface DispatchControl
 
     /**
      * Dispatches the message to the client.
+	 * @param networkInfo the message socket network information
      * @param message the message to dispatch.
      */
-    void dispatchInboundMessage(Message message);
+    void dispatchInboundMessage(NetworkInfo networkInfo, Message message);
 }
