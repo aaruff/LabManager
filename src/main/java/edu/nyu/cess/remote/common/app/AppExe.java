6402431588460c65fea6fca215708564495c0078
Anwar Ruff <anwarruff@gmail.com>
2016-03-07 14:49:04 -0500

    Added an error type and message to the application execution class

--- src/main/java/edu/nyu/cess/remote/common/app/AppExe.java
+++ src/main/java/edu/nyu/cess/remote/common/app/AppExe.java
@@ -6,52 +6,53 @@ public class AppExe implements Serializable
 {
 	private static final long serialVersionUID = -3416367781462816040L;
 
-	private final String name;
-	private final String path;
-	private final String args;
-
+	private final AppInfo appInfo;
 	private final AppState appState;
-
-	public AppExe(String name, String path, String args, AppState appState) {
-		this.name = name;
-		this.path = path;
-		this.args = args;
-
-		this.appState = appState;
-	}
+	private final ErrorType errorType;
+	private final String errorMessage;
 
 	public AppExe(AppInfo appInfo, AppState appState)
 	{
-		this.name = appInfo.getName();
-		this.path = appInfo.getPath();
-		this.args = appInfo.getOptions();
+		this.appInfo = appInfo;
 		this.appState = appState;
+		this.errorType = ErrorType.NO_ERROR;
+		this.errorMessage = "";
+	}
+
+	public AppExe(AppInfo appInfo, AppState appState, ErrorType errorType, String errorMessage)
+	{
+		this.appInfo = appInfo;
+		this.appState = appState;
+		this.errorType = errorType;
+		this.errorMessage = errorMessage;
 	}
 
 	public AppState getState() {
 		return appState;
 	}
 
-	public String getName() {
-		return name;
+	public AppInfo getAppInfo()
+	{
+		return appInfo;
 	}
 
-	public String getPath() {
-		return path;
-	}
-
-	public String getArgs() {
-		return args;
-	}
-
-    public boolean isSameApp(AppExe appExe)
+    public boolean isSameAppSameState(AppExe appExe)
     {
-        return name.equals(appExe.getName()) && path.equals(appExe.getPath()) && args.equals(appExe.getArgs());
+        return appInfo.equals(appExe.getAppInfo()) && appState == appExe.getState();
     }
 
-    public boolean isSameState(AppExe appExe)
+	@Override public String toString()
     {
-        return appExe.getState() == appState;
+        return "{appInfo=\"" + appInfo.toString() + "\", state=\"" + appState.toString() +"\"}";
     }
 
+	public ErrorType getErrorType()
+	{
+		return errorType;
+	}
+
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
 }
