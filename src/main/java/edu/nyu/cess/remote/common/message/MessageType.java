764c2ef041fb6076f411c625e60a24693ffc1306
Anwar Ruff <anwarruff@gmail.com>
2016-03-07 14:52:36 -0500

    renamed enum properties and removed NETWORK_INFO_UPDATE
    NETWORK_INFO_UPDATE was replaced by APP_EXE_UPDATE

--- src/main/java/edu/nyu/cess/remote/common/message/MessageType.java
+++ src/main/java/edu/nyu/cess/remote/common/message/MessageType.java
@@ -2,5 +2,5 @@ package edu.nyu.cess.remote.common.message;
 
 public enum MessageType
 {
-	APP_EXE_REQUEST, NETWORK_INFO_UPDATE, APP_EXE_UPDATE, KEEP_ALIVE_PING
+	APP_EXE_REQUEST, APP_EXE_UPDATE, KEEP_ALIVE_PING
 }
