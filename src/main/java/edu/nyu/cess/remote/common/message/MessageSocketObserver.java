8fde16bb5834a486a4d917acb0b10a52ace41267
Anwar Ruff <anwarruff@gmail.com>
2016-03-07 14:51:25 -0500

    Renamed class

--- src/main/java/edu/nyu/cess/remote/common/message/MessageSocketObserver.java
+++ src/main/java/edu/nyu/cess/remote/common/message/MessageSocketObserver.java
@@ -1,11 +1,14 @@
 package edu.nyu.cess.remote.common.message;
 
+import edu.nyu.cess.remote.common.net.ConnectionState;
 import edu.nyu.cess.remote.common.net.NetworkInfo;
 
 /**
  * Created by aruff on 2/8/16.
  */
-public interface MessageObserver
+public interface MessageSocketObserver
 {
     void notifyMessageReceived(NetworkInfo networkInfo, Message message);
+
+	void notifyMessageSenderState(ConnectionState connectionState);
 }
