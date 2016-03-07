f776205da2371e05dd970171f174d218ba6fd1bd
Anwar Ruff <anwarruff@gmail.com>
2016-03-07 14:59:02 -0500

    Deleted unused classes

--- src/main/java/edu/nyu/cess/remote/server/net/ClientSocket.java
+++ src/main/java/edu/nyu/cess/remote/server/net/ClientSocket.java
@@ -1,12 +1,8 @@
 package edu.nyu.cess.remote.server.net;
 
-import edu.nyu.cess.remote.common.app.AppExe;
 import edu.nyu.cess.remote.common.message.Message;
-import edu.nyu.cess.remote.common.message.MessageObserver;
 import edu.nyu.cess.remote.common.message.MessageSocket;
-import edu.nyu.cess.remote.common.message.MessageType;
 import edu.nyu.cess.remote.common.net.NetworkInfo;
-import edu.nyu.cess.remote.server.client.ClientDisconnectionObserver;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
@@ -14,123 +10,89 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ClientSocket implements MessageSocket, MessageObserver, ClientDisconnectionObserver, ClientHostNameObservable, AppExeSocket
+public class ClientSocket implements MessageSocket
 {
 	final static Logger logger = Logger.getLogger(ClientSocket.class);
 
-	private Socket socket;
-    private volatile SocketState socketState;
-    private volatile boolean networkInfoConfirmed;
+	private final Object inboundSocketLock = new Object();
+	private final Object outboundSocketLock = new Object();
 
-    private ClientSocketObserver clientSocketObserver;
+	private Socket socket;
 
     private volatile NetworkInfo networkInfo;
 
-    public ClientSocket(Socket socket, ClientSocketObserver clientSocketObserver)
+    public ClientSocket(NetworkInfo networkInfo, Socket socket)
     {
-        this.socketState = SocketState.CONNECTED;
-        this.socket = socket;
-        this.clientSocketObserver = clientSocketObserver;
-        this.networkInfoConfirmed = false;
-
-        startNetworkMonitorThreads();
+		this.networkInfo = networkInfo;
+		this.socket = socket;
     }
 
-	/**
-	 * {@link MessageObserver}
-	 */
-	@Override synchronized public void notifyMessageReceived(Message message)
-	{
-		switch(message.getMessageType()) {
-			case NETWORK_INFO_UPDATE:
-				if ( ! networkInfoConfirmed) {
-					String hostName = message.getNetworkInfo().getClientHostName();
-					networkInfo = new NetworkInfo(hostName, networkInfo.getClientIp(), networkInfo.getServerIp());
-				}
-			case APP_EXE_UPDATE:
-				AppExe appExe = message.getAppExe();
-				break;
-			case APP_EXE_REQUEST:
-				// Ignore
-				break;
-			case KEEP_ALIVE_PING:
-				// Ignore
-			default:
-				break;
+    /**
+     * {@link MessageSocket}
+     */
+    @Override public Message readMessage() throws IOException
+    {
+		synchronized (inboundSocketLock) {
+			if (!socket.isConnected()) {
+				throw new IOException("Attempting to read message from an disconnected socket.");
+			}
+
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+			Object object;
+			try {
+				object = objectInputStream.readObject();
+			} catch (ClassNotFoundException e) {
+				throw new IOException(e);
+			}
+			return (Message) object;
+		}
+    }
+
+    /**
+     * {@link MessageSocket}
+     */
+	@Override public void sendMessage(Message message) throws IOException
+    {
+		synchronized (outboundSocketLock) {
+			if ( ! socket.isConnected()) {
+				throw new IOException("Attempting to send an error message using a disconnected socket.");
+			}
+
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectOutputStream.writeObject(message);
+			objectOutputStream.flush();
 		}
 	}
 
-    /**
-     * {@link MessageSocket}
-     */
-    @Override synchronized public Message readMessage() throws IOException
-    {
-        if ( ! socket.isConnected()) {
-            throw new IOException("Attempting to read message from an disconnected socket.");
-        }
-
-        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-
-        Object object;
-        try {
-            object = objectInputStream.readObject();
-        } catch (ClassNotFoundException e) {
-            throw new IOException(e);
-        }
-        Message message = (Message) object;
-
-        return message;
-    }
-
-    /**
-     * {@link MessageSocket}
-     */
-	@Override synchronized public void sendMessage(Message message) throws IOException
-    {
-        if ( ! socket.isConnected()) {
-            throw new IOException("Attempting to send an error message using a disconnected socket.");
-        }
-
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-        objectOutputStream.writeObject(message);
-        objectOutputStream.flush();
+	/**
+	 * {@link MessageSocket}
+	 */
+	public NetworkInfo getNetworkInfo()
+	{
+		return networkInfo;
 	}
 
     /**
      * {@link MessageSocket}
      */
-    @Override synchronized public boolean isConnected()
+    @Override public boolean isConnected()
     {
         return socket.isConnected();
     }
 
 	/**
-	 * {@link ClientDisconnectionObserver}
+	 * {@link MessageSocket}
 	 */
-    @Override synchronized public void notifyClientDisconnected(NetworkInfo networkInfo)
+    @Override public String getClientName()
     {
-        socketState = SocketState.DISCONNECTED;
-
-        clientSocketObserver.notifyClientConnectionLost();
-        try {
-            socket.close();
-        } catch (IOException e) {
-            logger.error("Failed to close the client socket.", e);
-        }
+        return networkInfo.getClientName();
     }
 
 	/**
 	 * {@link MessageSocket}
 	 */
-    @Override synchronized public String getClientHostName()
-    {
-        return networkInfo.getClientHostName();
-    }
-
-	/**
-	 * {@link MessageSocket}
-	 */
-    @Override synchronized public String getClientIp()
+    @Override public String getClientIp()
     {
         return networkInfo.getClientIp();
     }
@@ -138,57 +100,8 @@ public class ClientSocket implements MessageSocket, MessageObserver, ClientDisco
 	/**
 	 * {@link MessageSocket}
 	 */
-    @Override synchronized public String getServerIp()
+    @Override public String getServerIp()
     {
-        return networkInfo.getClientHostName();
+        return networkInfo.getClientName();
     }
-
-	/**
-	 * {@link ClientHostNameObservable}
-	 */
-    @Override synchronized public boolean isHostNameSet()
-    {
-        return networkInfo.getClientHostName().isEmpty();
-    }
-
-	/**
-	 * {@link ClientHostNameObservable}
-	 */
-    @Override public void notifyHostNameConfirmed()
-    {
-        clientSocketObserver.notifyClientConfirmed();
-    }
-
-	/**
-	 * {@link AppExe}
-     */
-	@Override public void sendAppExe(AppExe appExe)
-	{
-		try {
-			sendMessage(new Message(MessageType.APP_EXE_REQUEST, appExe, networkInfo));
-		}
-		catch (IOException e) {
-			logger.error("Failed to send an execution message.", e);
-		}
-	}
-
-	private void startNetworkMonitorThreads()
-	{
-		String clientIp = socket.getInetAddress().getHostAddress();
-		String serverIp = socket.getLocalAddress().getHostAddress();
-		this.networkInfo = new NetworkInfo("", clientIp, serverIp);
-
-		Thread messageMonitorThread = new Thread(new MessageMonitorThread(this, this));
-		messageMonitorThread.setName("Inbound Message Monitor Thread");
-		messageMonitorThread.start();
-
-		Thread clientConnectionMonitorThread = new Thread(new ClientConnectionMonitor(this, this));
-		clientConnectionMonitorThread.setName("Client Connection Monitor Thread");
-		clientConnectionMonitorThread.start();
-
-		Thread clientHostNameConfirmationThread = new Thread(new ClientHostNameConfirmationRunnable(this, this));
-		clientHostNameConfirmationThread.setName("Client Network Info Update Thread");
-		clientHostNameConfirmationThread.start();
-	}
-
 }
