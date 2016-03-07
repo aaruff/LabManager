df595fc7b068b97682c2a1cae784f1478614583f
Anwar Ruff <anwarruff@gmail.com>
2016-03-07 15:00:37 -0500

    Added message dialog display of any errors that occur while loading config files

--- src/main/java/edu/nyu/cess/remote/server/Main.java
+++ src/main/java/edu/nyu/cess/remote/server/Main.java
@@ -3,9 +3,12 @@
  */
 package edu.nyu.cess.remote.server;
 
-import edu.nyu.cess.remote.server.app.AppInfoCollection;
+import edu.nyu.cess.remote.server.app.ClientAppInfoCollection;
+import edu.nyu.cess.remote.server.gui.ViewController;
 import edu.nyu.cess.remote.server.io.AppProfilesFile;
-import edu.nyu.cess.remote.server.client.ClientSocketPoolManager;
+import edu.nyu.cess.remote.server.client.ClientPoolProxy;
+import edu.nyu.cess.remote.server.io.LabLayoutFile;
+import edu.nyu.cess.remote.server.lab.LabLayout;
 import edu.nyu.cess.remote.server.net.ClientSocketConnectionMonitor;
 import edu.nyu.cess.remote.server.yaml.YamlExceptionMessage;
 import org.apache.log4j.Logger;
@@ -24,32 +27,48 @@ public class Main
 
     public static void main(String[] args)
     {
-        AppInfoCollection appInfoCollection = null;
+        ClientAppInfoCollection clientAppInfoCollection = null;
         try {
             InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("app-config.yaml");
-            appInfoCollection = AppProfilesFile.readFile(inputStream);
+			if (inputStream == null) {
+				JOptionPane.showMessageDialog(new JPanel(), "App config file not found.\n", "Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+
+            clientAppInfoCollection = AppProfilesFile.readFile(inputStream);
         }
         catch (YAMLException e) {
             JOptionPane.showMessageDialog(new JPanel(), YamlExceptionMessage.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             logger.error("YAML Exception: Unable to read config file because of an invalid entry(s).", e);
             System.exit(0);
         }
-        catch (NullPointerException e) {
-            JOptionPane.showMessageDialog(new JPanel(),
-                    "Could not open, or load, the configuration file.\n" + e.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            logger.error("File Not Found Exception: Unable to find the config file.", e);
-            System.exit(0);
-        }
 
-		ClientSocketPoolManager clientPoolManager = new ClientSocketPoolManager();
-        ViewController viewController = new ViewController(appInfoCollection, clientPoolManager, clientPoolManager);
+		LabLayout labLayout = null;
+		try {
+			InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("lab-layout.yaml");
+			if (inputStream == null) {
+				JOptionPane.showMessageDialog(new JPanel(), "Lab config file not found.\n", "Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+			labLayout = LabLayoutFile.readFile(inputStream);
+		}
+		catch (YAMLException e) {
+			JOptionPane.showMessageDialog(new JPanel(), YamlExceptionMessage.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			logger.error("YAML Exception: Unable to read the lab config file", e);
+			System.exit(0);
+		}
+
+		ClientPoolProxy clientPoolProxy = new ClientPoolProxy();
+        ViewController viewController = new ViewController(clientAppInfoCollection, clientPoolProxy, labLayout);
+
+		clientPoolProxy.addObserver(viewController);
+
         viewController.display();
 
 
-        ClientSocketConnectionMonitor messageObserver = new ClientSocketConnectionMonitor(clientPoolManager);
+        ClientSocketConnectionMonitor messageObserver = new ClientSocketConnectionMonitor(clientPoolProxy);
 		try {
-			messageObserver.monitorNewClientSocketConnections(2600);
+			messageObserver.monitorNewClientSocketConnections(2600, labLayout);
 		} catch (IOException e) {
 			logger.error("Failed to initialize the server socket.", e);
 			System.exit(0);
