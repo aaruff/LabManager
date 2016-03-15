40693cdf0b1721b53c8802fb5f267f3d8f025f03
Anwar Ruff <anwarruff@gmail.com>
2016-03-15 14:13:51 -0400

    Removed unused constructor and getter

--- src/main/java/edu/nyu/cess/remote/common/message/Message.java
+++ src/main/java/edu/nyu/cess/remote/common/message/Message.java
@@ -12,19 +12,12 @@ public class Message implements Serializable
 	private MessageType messageType;
 	private NetworkInfo networkInfo;
 	private AppExe appExe;
-	private String clientMessage;
 
 	public Message(MessageType messageType, AppExe appExe, NetworkInfo networkInfo)
 	{
 		this.messageType = messageType;
 		this.appExe = appExe;
 		this.networkInfo = networkInfo;
-	}
-
-	public Message(MessageType messageType, String clientMessage)
-	{
-		this.messageType = messageType;
-		this.clientMessage = clientMessage;
 	}
 
 	public Message(MessageType messageType, NetworkInfo networkInfo)
@@ -45,10 +38,5 @@ public class Message implements Serializable
 	public AppExe getAppExe()
 	{
 		return appExe;
-	}
-
-	public String getClientMessage()
-	{
-		return clientMessage;
 	}
 }
