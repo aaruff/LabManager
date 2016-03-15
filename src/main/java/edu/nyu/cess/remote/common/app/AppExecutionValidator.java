0d7fc1e558df0e6ca1e1af4e19929918c97fd81a
Anwar Ruff <anwarruff@gmail.com>
2016-03-15 14:13:31 -0400

    Implemented validation error message getter, and removed unused function

--- src/main/java/edu/nyu/cess/remote/common/app/AppExecutionValidator.java
+++ src/main/java/edu/nyu/cess/remote/common/app/AppExecutionValidator.java
@@ -9,16 +9,26 @@ public class AppExecutionValidator
 		}
 
 		AppInfo info = exe.getAppInfo();
-        return ! (info == null || isEmpty(info.getName()) || isEmpty(info.getPath()) || isEmpty(info.getArgs()) || isNotState(exe.getState()));
+        return ! (info == null || isEmpty(info.getName()) || isEmpty(info.getPath()) || info.getArgs() == null || exe.getState() == null);
     }
+
+	public static String getValidationError(AppExe exe)
+	{
+		if (exe == null) {
+			return "App Execution Null";
+		}
+
+		AppInfo info = exe.getAppInfo();
+		if (info == null) {
+			return "App Info Null";
+		}
+
+		return String.format("Error Empty or null field found in: name=%b, path=%b, args=%b, state=%b",
+				isEmpty(info.getName()), isEmpty(info.getPath()), info.getArgs() == null, exe.getState() == null);
+	}
 
     private static boolean isEmpty(String text)
     {
         return (text == null || text.isEmpty());
-    }
-
-    private static boolean isNotState(AppState state)
-    {
-        return state == null;
     }
 }
